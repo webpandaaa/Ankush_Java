c09b9c071bbb4b6db5f7c2433a43ce93c5f10654
Singh <10020823@ergo.com>
2026-01-02 11:42:16 +0530

    first one

---
+++ Array/p14.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class p14 {
+    public static void main(String args[]){
+        int arr[] = {1,2,3,4,5};
+        printNum(arr);
+    }
+
+    private static void printNum(int[] arr) {
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
